096b70b4955d490a49e27e1aaffde6f2996f8c20
gurleensdogsrock <87973986+gurleensdogsrock@users.noreply.github.com>
2021-12-23 16:04:57 +1000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C45-make-your-own-gurleen-
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(1000, 600);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
